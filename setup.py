0a4566501e8d37aa120f2d790971b9988490f68a
francois-le-ko4la <francois@le.ko4la.fr>
2018-06-02 15:07:21 +0200

    test python version

--- setup.py
+++ setup.py
@@ -15,7 +15,11 @@ Test:
 from setuptools import setup, find_packages
 from setuptools.config import read_configuration
 import warnings
+import sys
 
+
+if not sys.version_info[0] == 3:
+        sys.exit("Sorry, your Python is not supported (yet)")
 
 warnings.filterwarnings("ignore")
 CFG = read_configuration('./setup.cfg')
